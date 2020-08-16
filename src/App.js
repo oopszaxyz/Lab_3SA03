d917ebec98bed4b6c9c00a3dada8ec65fd88b280
oopszaxyz <6110110301@psu.ac.th>
2020-08-16 11:00:35 +0700

    part 7.4

--- src/App.js
+++ src/App.js
@@ -4,15 +4,40 @@ import './App.css';
 import CharacterCard from './CharacterCard';
 import WordCard from './WordCard';
 
-const word = "Hello";
+var hint;
+var word;
+var Random = Math.floor(Math.random() * 3) + 1;
+
+if (Random == 1){
+  word = "Hello";
+  hint = "The words that you say when greeting";
+  
+}
+if (Random == 2){
+  word = "Lions";
+  hint = "The king of forest and more then one";
+}
+if (Random == 3){
+  word = "Orange";
+  hint ="The results are round and the name is the same as the color.";
+}
+
+
 function App() {
   return (
     <div>
       <h1 className = 'gameName'>
         Welcome to Word Spelling Game
       </h1>
+
+      <h1 className = 'gameHint'>
+        Hint : {hint}
+      </h1>
+
       <WordCard value={word}/>
+
     </div>
   );
  }
+
  export default App;
